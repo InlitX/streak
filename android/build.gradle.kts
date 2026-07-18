b320faf93dd4c9e2613ccecb499bc4518f30860b
InlitX <inlitxx@gmail.com>
2026-07-18 18:37:33 +0200

    CI: align all Gradle modules to JVM 11

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -33,6 +33,25 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Plugins like home_widget default to JVM 1.8 but inline JVM-11 bytecode from
+// Glance/Compose; align every module to 11, matching :app.
+subprojects {
+    val alignJvm: Project.() -> Unit = {
+        extensions.findByType(com.android.build.gradle.LibraryExtension::class.java)
+            ?.compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_11
+                targetCompatibility = JavaVersion.VERSION_11
+            }
+        tasks.withType<JavaCompile>().configureEach {
+            sourceCompatibility = "11"; targetCompatibility = "11"
+        }
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            compilerOptions { jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11) }
+        }
+    }
+    if (state.executed) alignJvm() else afterEvaluate { alignJvm() }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
